da39177fcf530d3201c432303534eddc8ce248bc
khanshorif331 <khanshorif331@gmail.com>
2023-01-30 00:16:01 +0600

    done for today

--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -2,8 +2,9 @@ import React, { useEffect, useState } from 'react'
 import loginImage from '../assets/login.svg'
 import { useForm, useWatch } from 'react-hook-form'
 import { useNavigate } from 'react-router-dom'
-import { useDispatch } from 'react-redux'
-import { createUser } from '../features/auth/authSlice'
+import { useDispatch, useSelector } from 'react-redux'
+import { createUser, googleLogin } from '../features/auth/authSlice'
+import { toast } from 'react-hot-toast'
 const Signup = () => {
 	const { handleSubmit, register, reset, control } = useForm()
 	const password = useWatch({ control, name: 'password' })
@@ -12,6 +13,7 @@ const Signup = () => {
 	const [disabled, setDisabled] = useState(true)
 	const dispatch = useDispatch()
 
+	const { isError, error } = useSelector(state => state.auth)
 	useEffect(() => {
 		if (
 			password !== undefined &&
@@ -25,10 +27,18 @@ const Signup = () => {
 			setDisabled(true)
 		}
 	}, [password, confirmPassword])
+	useEffect(() => {
+		if (isError) {
+			toast.error(error)
+		}
+	}, [isError, error])
 
 	const onSubmit = ({ email, password }) => {
 		dispatch(createUser({ email, password }))
 		console.log('email', email)
+	}
+	const handleGoogleLogin = () => {
+		dispatch(googleLogin())
 	}
 
 	return (
@@ -94,6 +104,13 @@ const Signup = () => {
 									</span>
 								</p>
 							</div>
+							<button
+								type="button"
+								onClick={handleGoogleLogin}
+								className="font-bold text-white py-3 rounded-full bg-primary w-full"
+							>
+								Login with Google
+							</button>
 						</div>
 					</form>
 				</div>
