0b8275d68dd4bc4b5dcb00ffc355072c3a5705ff
khanshorif331 <khanshorif331@gmail.com>
2023-01-29 23:40:43 +0600

    login logout done

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,11 +1,12 @@
 import React, { useEffect } from 'react'
 import { useForm } from 'react-hook-form'
-import { useDispatch } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 
 import { useNavigate } from 'react-router-dom'
 import loginImage from '../assets/login.svg'
 import { loginUser } from '../features/auth/authSlice'
 const Login = () => {
+	const { isLoading, email } = useSelector(state => state.auth)
 	const { register, handleSubmit, reset } = useForm()
 	const navigate = useNavigate()
 	const dispatch = useDispatch()
@@ -13,6 +14,12 @@ const Login = () => {
 	const onSubmit = ({ email, password }) => {
 		dispatch(loginUser({ email, password }))
 	}
+
+	useEffect(() => {
+		if (!isLoading && email) {
+			navigate('/')
+		}
+	}, [isLoading, email, navigate])
 
 	return (
 		<div className="flex h-screen items-center">
