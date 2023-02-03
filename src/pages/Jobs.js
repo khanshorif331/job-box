1178272f0501850e9e8db2c97f66d19d38a5692f
khanshorif331 <khanshorif331@gmail.com>
2023-02-03 16:35:48 +0600

    job details page done with bug fixing

--- src/pages/Jobs.js
+++ src/pages/Jobs.js
@@ -1,17 +1,23 @@
-import React from "react";
-import JobCard from "../components/reusable/JobCard";
+import React from 'react'
+import { useSelector } from 'react-redux'
+import JobCard from '../components/reusable/JobCard'
+import { useGetJobsQuery } from '../features/job/jobApi'
 
 const Jobs = () => {
-  return (
-    <div className='pt-14'>
-      <div className='bg-primary/10 p-5 rounded-2xl'>
-        <h1 className='font-semibold text-xl'>Find Jobs</h1>
-      </div>
-      <div className='grid grid-cols-2 gap-5 mt-5'>
-        <JobCard />
-      </div>
-    </div>
-  );
-};
+	const { data, isLoading, isError } = useGetJobsQuery()
+	console.log(data?.data)
+	return (
+		<div className="pt-14">
+			<div className="bg-primary/10 p-5 rounded-2xl">
+				<h1 className="font-semibold text-xl">Find Jobs</h1>
+			</div>
+			<div className="grid grid-cols-1 md:grid-cols-2 gap-5 mt-5 p-5">
+				{data?.data?.map(job => (
+					<JobCard jobData={job} />
+				))}
+			</div>
+		</div>
+	)
+}
 
-export default Jobs;
+export default Jobs
