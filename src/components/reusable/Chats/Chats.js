b00b2598f32f9e718c70b64cf1e91e3b4dff61ed
khanshorif331 <khanshorif331@gmail.com>
2023-03-01 01:09:51 +0600

    getting data from db with useGet chats by email hook

--- src/components/reusable/Chats/Chats.js
+++ src/components/reusable/Chats/Chats.js
@@ -1,6 +1,14 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
+import { useGetChatsByEmailQuery } from '../../../features/chat/chatApi'
 
 const Chats = () => {
+	const { role, email } = useSelector(state => state.auth.user)
+	const { data } = useGetChatsByEmailQuery({
+		role,
+		email,
+	})
+	console.log(data)
 	return (
 		<div>
 			<h1>This is chats</h1>
